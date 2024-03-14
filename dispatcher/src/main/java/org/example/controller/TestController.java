3e413017c8f1bbf96d0b6d98ad744003688edaf2
Omest982 <93486447+Omest982@users.noreply.github.com>
2024-03-14 16:45:30 +0200

    Added eureka server service. Added api-gateway service.

--- dispatcher/src/main/java/org/example/controller/TestController.java
+++ dispatcher/src/main/java/org/example/controller/TestController.java
@@ -1,11 +1,13 @@
 package org.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class TestController {
-    @GetMapping
+    @GetMapping("/hello")
     public String test(){
         return "All works!";
     }
