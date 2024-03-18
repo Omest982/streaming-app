51f1297912632d3366965ff45303daf0b161f357
Omest982 <93486447+Omest982@users.noreply.github.com>
2024-03-18 19:18:37 +0200

    Created filter for eureka.

---
+++ eureka-service/src/main/java/org/example/config/EurekaConfig.java
@@ -0,0 +1,28 @@
+package org.example.config;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class EurekaConfig {
+
+    @Bean
+    public FilterRegistrationBean<?> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("Accept");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
